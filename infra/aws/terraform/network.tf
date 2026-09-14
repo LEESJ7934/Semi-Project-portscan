5f1851aaf8beddb835f3c59377631f8770826104
seungjun <sj7934@gmail.com>
2026-09-14 18:23:29 +0900

    chore: format AWS Terraform configuration

--- infra/aws/terraform/network.tf
+++ infra/aws/terraform/network.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "lab" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "portscan-lab-vpc" }
+  tags                 = { Name = "portscan-lab-vpc" }
 }
 
 resource "aws_internet_gateway" "lab" {
